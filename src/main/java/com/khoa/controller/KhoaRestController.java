253b5e1025b181239b9937e3b263830eca0b6405
catluynh <catluynh99@gmail.com>
2021-10-21 10:26:01 +0700

    update project

--- src/main/java/com/khoa/controller/KhoaRestController.java
+++ src/main/java/com/khoa/controller/KhoaRestController.java
@@ -3,6 +3,7 @@ package com.khoa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +20,23 @@ import com.khoa.service.KhoaService;
 
 @RestController
 @RequestMapping("/khoa")
-public class SinhVienRestController {
+public class KhoaRestController {
 	@Autowired
 	private KhoaService khoaService;
-	
+
+//	@Value("${eureka.client.instance.hostname}")
+//	private String welcome;
+
 	@GetMapping("")
 	public List<Khoa> findAll(){
 		return khoaService.findAll();
 	}
-	
+
+	@GetMapping("/welcome")
+	public String welcome(@Value("${hello}") String welcome){
+		return welcome;
+	}
+
 	@GetMapping("{id}")
 	public Khoa findAll(@PathVariable int id){
 		return khoaService.findById(id);
